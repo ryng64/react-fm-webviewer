86223a04792545158a68c976bc27b8b3fab1cd76
Ryan <32664992+ryng64@users.noreply.github.com>
2024-02-12 14:50:10 -0800

    cleaned up upload to FM script

--- uploadToFM.js
+++ uploadToFM.js
@@ -5,27 +5,14 @@ import fs from "fs";
 function uploadToFM() {
   const fmFile = `webviewdeploy.fmp12`;
   const fmScript = `uploadHTMLFile`;
-  let filePath = "";
-  let fmpURL = "";
-  fs.readdir("dist/", (err, files) => {
-    files.forEach((file) => {
-      if (path.extname(file) == ".html") {
-        console.log(path.resolve(`dist/${file}`));
-        // filePath = path.resolve(`dist/${file}`).replace(path.extname(file), "");
-        fmpURL = `fmp://$/${fmFile}?script=${fmScript}`;
-
-        if (fmpURL == "") return;
-        const command = `start ${fmpURL}`;
-        console.log(command);
-        exec(command, (err, stdout, stderr) => {
-          if (err) {
-            return;
-          }
-          // console.log(`stdout: ${stdout}`);
-          // console.log(`stderr: ${stderr}`);
-        });
-      }
-    });
+  let fmpURL = `fmp://$/${fmFile}?script=${fmScript}`;
+  const command = `start ${fmpURL}`;
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      return;
+    }
+    // console.log(`stdout: ${stdout}`);
+    // console.log(`stderr: ${stderr}`);
   });
 }
 
